b02b0eb2e150d0045522fbfda6468a05e1a37678
Rodrigo Cobos <101303234+rodrigomcobos@users.noreply.github.com>
2024-09-08 23:58:42 -0600

    working on hero component'

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import { useState, useEffect } from "react"
-import Hero from "./components/Hero"
-
+import { useState } from 'react'
+import Hero from './components/Hero'
+import Generator from './components/Generator'
+import Workout from './components/Workout'
 const App = () => {
   return (
-    <div>App</div>
+    <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
+      <Hero />
+      <Generator />
+      <Workout />
+    </main>
   )
 }
 
